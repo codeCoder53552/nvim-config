90aed00dd1b327c7e6034da1931023b7dc0bea9b
Colby Weber <cweber@wiktel.com>
2023-11-30 01:16:05 -0700

    easy command enter

--- lua/myLua/remap.lua
+++ lua/myLua/remap.lua
@@ -18,3 +18,5 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>Y', '"+y')
 vim.keymap.set('n', '<leader>Y', '"+Y')
+
+vim.keymap.set('n', '!', ':!')
